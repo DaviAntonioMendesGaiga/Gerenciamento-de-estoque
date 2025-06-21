2b90bb7bffa83fd5489034eff74eebbd5fb8f6a2
Davi Antonio Mendes Gaiga <davigaiga01@gmail.com>
2025-06-21 20:56:41 -0300

    Adicionando funcionalidade de navegação a sidebar e responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,62 @@
-import './App.css';
+import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Sidebar from "./components/Sidebar/Sidebar";
-import Produto from './components/Produto/Produto';
-import DivisorCatg from './components/DivisorCatg/DivisorCatg';
-import Entradas from './components/Entradas/Entradas';
-import Vendas from './components/Vendas/Vendas';
-import Relatorios from './components/Relatórios/Relatorios';
-import BuscarProdutos from './components/BuscarProdutos/BuscarProdutos';
-
-import { Element } from 'react-scroll';
+import Produto from "./components/Produto/Produto";
+import DivisorCatg from "./components/DivisorCatg/DivisorCatg";
+import Entradas from "./components/Entradas/Entradas";
+import Vendas from "./components/Vendas/Vendas";
+import Relatorios from "./components/Relatórios/Relatorios";
+import BuscarProdutos from "./components/BuscarProdutos/BuscarProdutos";
+import { Element } from "react-scroll";
 
 function App() {
   return (
-    <div className='app-container'>
-      <Sidebar />
-      <main id="scroll-container" className='main-content'>
-        <Element name="dashboard" className='section'>
-          <h1>Dashboard</h1>
-          <Dashboard />
-        </Element>
+    <>
+      <div className="app-container">
+        <Sidebar />
+        <main className="main-content" id="scroll-container">
+          <Element name="dashboard">
+            <h1>Dashboard</h1>
+            <Dashboard />
+          </Element>
 
-        <DivisorCatg />
+          <DivisorCatg />
 
-        <Element name="produtos" className='section'>
-          <h1 className='h1-margin'>Produtos</h1>
-          <Produto />
-        </Element>
+          <Element name="produtos">
+            <h1 className="h1-margin">Produtos</h1>
+            <Produto />
+          </Element>
 
-        <DivisorCatg />
+          <DivisorCatg />
 
-        <Element name="entradas" className='section'>
-          <h1 className='h1-margin'>Entradas</h1>
-          <Entradas />
-        </Element>
+          <Element name="entradas">
+            <h1 className="h1-margin">Entradas</h1>
+            <Entradas />
+          </Element>
 
-        <DivisorCatg />
+          <DivisorCatg />
 
-        <Element name="vendas" className='section'>
-          <h1 className='h1-margin'>Vendas</h1>
-          <Vendas />
-        </Element>
+          <Element name="vendas">
+            <h1 className="h1-margin">Vendas</h1>
+            <Vendas />
+          </Element>
 
-        <DivisorCatg />
+          <DivisorCatg />
 
-        <Element name="relatorios" className='section'>
-          <h1 className='h1-margin'>Relatórios</h1>
-          <Relatorios />
-        </Element>
+          <Element name="relatorios">
+            <h1 className="h1-margin">Relatórios</h1>
+            <Relatorios />
+          </Element>
 
-        <DivisorCatg />
+          <DivisorCatg />
 
-        <Element name="buscar" className='section'>
-          <h1 className='h1-margin'>Buscar Produtos</h1>
-          <BuscarProdutos />
-        </Element>
-      </main>
-    </div>
+          <Element name="buscar">
+            <h1 className="h1-margin">Buscar Produtos</h1>
+            <BuscarProdutos />
+          </Element>
+        </main>
+      </div>
+    </>
   );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,97 +6,124 @@ import {
   BarChart,
   Search,
   LogOut,
+  Menu,
+  X,
 } from "lucide-react";
 import classes from "./sidebar.module.css";
 import GSGerenciamentosLogo from "../../assets/GSGerenciamentosLogo.png";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
-    <div className={classes.sidebar}>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <img src={GSGerenciamentosLogo} alt="Logo" />
-          </li>
-          <li>
-            <Link
-              to="dashboard"
-              containerId="scroll-container"
-              smooth={true}
-              duration={600}
-              offset={-40}
-              className={classes.navLink}
-            >
-              <LayoutDashboard size={20} /> Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="produtos"
-              containerId="scroll-container"
-              smooth={true}
-              duration={600}
-              offset={-40}
-              className={classes.navLink}
-            >
-              <Package size={20} /> Produtos
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="entradas"
-              containerId="scroll-container"
-              smooth={true}
-              duration={600}
-              offset={-40}
-              className={classes.navLink}
-            >
-              <ArrowDownCircle size={20} /> Entradas
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="vendas"
-              containerId="scroll-container"
-              smooth={true}
-              duration={600}
-              offset={-40}
-              className={classes.navLink}
-            >
-              <ShoppingCart size={20} /> Vendas
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="relatorios"
-              containerId="scroll-container"
-              smooth={true}
-              duration={600}
-              offset={-40}
-              className={classes.navLink}
-            >
-              <BarChart size={20} /> Relatórios
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="buscar"
-              containerId="scroll-container"
-              smooth={true}
-              duration={600}
-              offset={-40}
-              className={classes.navLink}
-            >
-              <Search size={20} /> Buscar Produtos
-            </Link>
-          </li>
-          <li className={classes.sairBtn}>
-            <LogOut size={20} /> Sair
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <button
+        className={classes.mobileMenuButton}
+        onClick={toggleMobileMenu}
+        aria-label={mobileMenuOpen ? "Fechar menu" : "Abrir menu"}
+      >
+        {mobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+      </button>
+
+      <div
+        className={`${classes.sidebar} ${
+          mobileMenuOpen ? classes.sidebarMobileOpen : ""
+        }`}
+      >
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <img src={GSGerenciamentosLogo} alt="Logo" />
+            </li>
+            <li>
+              <Link
+                to="dashboard"
+                containerId="scroll-container"
+                smooth={true}
+                duration={600}
+                offset={-40}
+                onClick={handleLinkClick}
+              >
+                <LayoutDashboard size={20} /> Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="produtos"
+                containerId="scroll-container"
+                smooth={true}
+                duration={600}
+                offset={-40}
+                onClick={handleLinkClick}
+              >
+                <Package size={20} /> Produtos
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="entradas"
+                containerId="scroll-container"
+                smooth={true}
+                duration={600}
+                offset={-40}
+                onClick={handleLinkClick}
+              >
+                <ArrowDownCircle size={20} /> Entradas
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="vendas"
+                containerId="scroll-container"
+                smooth={true}
+                duration={600}
+                offset={-40}
+                onClick={handleLinkClick}
+              >
+                <ShoppingCart size={20} /> Vendas
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="relatorios"
+                containerId="scroll-container"
+                smooth={true}
+                duration={600}
+                offset={-40}
+                onClick={handleLinkClick}
+              >
+                <BarChart size={20} /> Relatórios
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="buscar"
+                containerId="scroll-container"
+                smooth={true}
+                duration={600}
+                offset={-40}
+                onClick={handleLinkClick}
+              >
+                <Search size={20} /> Buscar Produtos
+              </Link>
+            </li>
+            <li className={classes.sairBtn}>
+              <LogOut size={20} /> Sair
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
 
